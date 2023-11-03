d2d269c20ea0f1c6d047b59d54d11ee4573b55f0
rudetm32 <jaruiz721818@gmail.com>
2023-11-03 00:12:03 +0000

    initial commit

---
+++ ejercicios/numeric-data.py
@@ -0,0 +1,30 @@
+print("Python has three numeric types: int, float, and complex")
+print("--------------------------------------------------------")
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print("--------------------------------------------------------")
+
+myValue=3.1416
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print("--------------------------------------------------------")
+
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print("--------------------------------------------------------")
+
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print("--------------------------------------------------------")
+
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
