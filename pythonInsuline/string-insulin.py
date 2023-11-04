28af65169c04b0ee32000a71bc5567ec77c6d9eb
rudetm32 <jaruiz721818@gmail.com>
2023-11-04 09:31:59 -0600

    refact(pythonInsuline/string-insulin.py): idioma comentarios

--- pythonInsuline/string-insulin.py
+++ pythonInsuline/string-insulin.py
@@ -1,43 +1,41 @@
-# Store the human preproinsulin sequence in a variable called preproinsulin:
+# Secuencia de preproinsulina humana
+preproInsulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktr" \
+                "reaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"
 
-preproInsulin ="malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktr" \
-"reaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"
+# Secuencias de insulina humana
+lsInsulin = "malwmrllpllallalwgpdpaaa"
+bInsulin = "fvnqhlcgshlvealylvcgergffytpkt"
+aInsulin = "giveqcctsicslyqlenycn"
+cInsulin = "rreaedlqvgqvelgggpgagslqplalegslqkr"
 
-# Store the remaining sequence elements of human insulin in variables:
+# Combinar las cadenas B y A para formar la secuencia completa de la insulina
+insulin = bInsulin + aInsulin
 
-lsInsulin="malwmrllpllallalwgpdpaaa"
-bInsulin="fvnqhlcgshlvealylvcgergffytpkt"
-aInsulin="giveqcctsicslyqlenycn"
-cInsulin="rreaedlqvgqvelgggpgagslqplalegslqkr"
-
-insulin= bInsulin + aInsulin
-
-
-# Printing "the sequence of human insulin" to console using successive print() commands:
-print("The sequence of human prepoinsuline: ")
+# Imprimir la secuencia de preproinsulina humana
+print("La secuencia de preproinsulina humana:")
 print(preproInsulin)
 
+# Imprimir la secuencia de la cadena A de la insulina humana
+print("La secuencia de la insulina humana, cadena A:", aInsulin)
 
-# Printing to console using concatenated strings inside the print function (one-liner):
-print("The sequence of human insulin, chain a: ", aInsulin)
-
-
-# Calculating the molecular weight of insulin  
-# Creating a list of the amino acid (AA) weights  
+# Diccionario de pesos moleculares de aminoácidos
 aaWeights = {'A': 89.09, 'C': 121.16, 'D': 133.10, 'E': 147.13, 'F': 165.19,
-'G': 75.07, 'H': 155.16, 'I': 131.17, 'K': 146.19, 'L': 131.17, 'M': 149.21,
-'N': 132.12, 'P': 115.13, 'Q': 146.15, 'R': 174.20, 'S': 105.09, 'T': 119.12,
-'V': 117.15, 'W': 204.23, 'Y': 181.19}  
-# Count the number of each amino acids  
-aaCountInsulin = ({x: float(insulin.upper().count(x)) for x in ['A', 'C',
-'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T',
-'V', 'W', 'Y']})  
-# Multiply the count by the weights  
-molecularWeightInsulin = sum({x: (aaCountInsulin[x]*aaWeights[x]) for x in
-['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R',
-'S', 'T', 'V', 'W', 'Y']}.values())  
-print("The rough molecular weight of insulin: " +
-str(molecularWeightInsulin))
+             'G': 75.07, 'H': 155.16, 'I': 131.17, 'K': 146.19, 'L': 131.17,
+             'M': 149.21, 'N': 132.12, 'P': 115.13, 'Q': 146.15, 'R': 174.20,
+             'S': 105.09, 'T': 119.12, 'V': 117.15, 'W': 204.23, 'Y': 181.19}
 
+# Calcular el número de cada aminoácido en la secuencia de insulina
+aaCountInsulin = {aa: float(insulin.upper().count(aa)) for aa in aaWeights.keys()}
+
+# Calcular el peso molecular de la insulina
+molecularWeightInsulin = sum(aaCountInsulin[aa] * weight for aa, weight in aaWeights.items())
+
+# Imprimir el peso molecular estimado de la insulina
+print("El peso molecular estimado de la insulina:", molecularWeightInsulin)
+
+# Peso molecular real de la insulina
 molecularWeightInsulinActual = 5807.63
-print("Error percentage: " + str(((molecularWeightInsulin - molecularWeightInsulinActual)/molecularWeightInsulinActual)*100))
+
+# Calcular y imprimir el porcentaje de error
+error_porcentaje = ((molecularWeightInsulin - molecularWeightInsulinActual) / molecularWeightInsulinActual) * 100
+print("Porcentaje de error:", error_porcentaje)
